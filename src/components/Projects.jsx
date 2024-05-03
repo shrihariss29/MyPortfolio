01f5e08245282c2812e95cde080f6c15744e92b6
Shrihari <140818388+shrihariss29@users.noreply.github.com>
2024-05-03 18:42:06 +0530

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,87 +1,75 @@
-const Projects = () => {
+import React from "react";
+import { Link } from "react-router-dom";
 
-  return <div class="mainprojects">
-    <p class="quote animate" style={{fontSize:"35px"}} >Works i did that excites me</p>
-      <div class="projectcard">
-
-      <div class="card" style={{backgroundColor: "#F2AFEF"}}>
-       <img src={require("../assets/Htmlpic.png")} class="card-img-top projimg" alt="Machine learning"/>
-         <div class="card-body">
-          <h5 class="card-title" style={{color: "brown"}}>Full stack quiz application with Node.js, Express,js and Postgres </h5>
-            <p class="card-text" >This is a backend project where the user's name is stored in the database initally and allows them
-            to play the fun quiz. After the quiz is finished, it will view the high score. Frontend is built with embedded java script
-            and backend is done with Node.js</p>
-             <a href="https://github.com/shrihariss29/Quish" target="_blank" class="btn btn-primary">GitHub link</a>
-          </div>
-       </div>
-
-      <div class="card" style={{backgroundColor: "blanchedalmond"}}>
-       <img src={require("../assets/Mldlpic.png")} class="card-img-top projimg" alt="Machine learning"/>
-         <div class="card-body">
-          <h5 class="card-title" style={{color: "brown"}}>Credit card fraud detection using machine learning and deep learning </h5>
-            <p class="card-text" >This is my project in which i took the several machine learning algorithm and convolutional neural
-             network and passed it through "creditcard.csv" dataset.I noticed convolutional nueral network perfroms better than the machine
-             learning algorithms taken.</p>
-             <a href="https://github.com/shrihariss29/Credit-card-fraud-project" target="_blank" class="btn btn-primary">GitHub link</a>
-          </div>
-       </div>
-
-       <div class="card" style={{backgroundColor: "#96EFFF"}}>
-       <img src={require("../assets/Htmlpic.png")} class="card-img-top projimg" alt="html tag"/>
-         <div class="card-body">
-          <h5 class="card-title" style={{color: "blue"}}>Portfolio website using react.js, HTML and CSS</h5>
-            <p class="card-text" >This project is done using react.js primarily. This portfolio contains all the necessary information 
-            about myself. It is hosted via GitHub pages.This has the scope of many future add-ons which can be added in future.</p>
-             <a href="https://github.com/shrihariss29/Portfolio-2" target="_blank" class="btn btn-primary">GitHub link</a>
-          </div>
-       </div>
-
-       <div class="card" style={{backgroundColor: "#C7DCA7"}}>
-       <img src={require("../assets/dapic.png")} class="card-img-top projimg" alt="html tag"/>
-         <div class="card-body">
-          <h5 class="card-title" style={{color: "green"}}>Data analysis using titanic.csv dataset</h5>
-            <p class="card-text">Data analysis using titanic dataset is a based on the sinking of titanic ship. It consists the data 
-            of passenger who travelled in the ship during the accident. Based on the specific passesger's sex,age,ticket type and other
-            parameters, chances of their survival rate is calculated.</p>
-             <a href="https://github.com/shrihariss29/Titanic-dataset-project" target="_blank" class="btn btn-primary">GitHub link</a>
-          </div>
-       </div>
+const ProjectCard = ({ title, description, githubLink, backgroundColor, imageSrc }) => {
+  return (
+    <div className="card" style={{ backgroundColor }}>
+      <img src={imageSrc} className="card-img-top projimg" alt="Project" />
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
+        <a href={githubLink} target="_blank" className="btn btn-primary">
+          GitHub link
+        </a>
       </div>
+    </div>
+  );
+};
 
-      <div class="card" style={{backgroundColor: "#DDE6ED"}}>
-       <img src={require("../assets/decisiontree.png")} class="card-img-top projimg" alt="ML certificate"/>
-         <div class="card-body">
-          <h5 class="card-title" style={{color: "Black"}}>Machine learning - basic to advanced (Certification)</h5>
-            <p class="card-text">This is a course in where i started to learn about machine learning and data analytics.
-            It is very fascinating to learn it, because it feels like we are predicting the future. Learnt the deep intricacies
-            and insights in this area.</p>
-            <a href={require("../assets/MLcertificate.png")} target="_blank" class="btn btn-primary">Certificate</a>
-          </div>
-       </div>
-
-       <div class="card" style={{backgroundColor: "#FFBF9B"}}>
-       <img src={require("../assets/excellogo.jpg")} class="card-img-top projimg" alt="excel"/>
-         <div class="card-body">
-          <h5 class="card-title" style={{color: "maroon"}}>Excel basics for data analytics (Certification)</h5>
-            <p class="card-text"> Working with Microsoft excel is a very important skill for a data analyst. Learnt many
-            applications of excel and felt that it is very underrated and useful tool. Had a chance to use it in my real
-            life applications also.</p>
-             <a href={require("../assets/Excelcertificate.png")} target="_blank" class="btn btn-primary">Certificate</a>
-          </div>
-       </div>
-
-      <div class="card" style={{backgroundColor: "#F8EDE3"}}>
-      <img src={require("../assets/Htmlpic.png")} class="card-img-top projimg" alt="html tag"/>
-        <div class="card-body">
-         <h5 class="card-title" style={{color: "#16213E"}}>The Complete 2023 Web Development Bootcamp (Certification)</h5>
-           <p class="card-text">The course which made me felt so excited as well as confident. I felt empowered and skilled after 
-           finishing this course. The feel of creating something new is surreal. Mrs.Angela Yu is the instructor of this course and 
-           i am very thankful for her. This website is developed by the skill which i gained there.</p>
-           <a href={require("../assets/udemy.jpg")} target="_blank" class="btn btn-primary">Certificate</a>
-         </div>
+const Projects = () => {
+  return (
+    <div className="main-projects">
+      <p className="quote animate" style={{ fontSize: "35px" }}>
+        Works I did that excites me
+      </p>
+      <div className="project-card">
+        <ProjectCard
+          title="Full stack quiz application with Node.js, Express.js, and Postgres"
+          description="This is a backend project where the user's name is stored in the database initially and allows them to play the fun quiz. After the quiz is finished, it will view the high score. Frontend is built with embedded JavaScript and backend is done with Node.js."
+          githubLink="https://github.com/shrihariss29/Quish"
+          backgroundColor="#F2AFEF"
+          imageSrc={require("../assets/Htmlpic.png")}
+        />
+        <ProjectCard
+          title="Credit card fraud detection using machine learning and deep learning"
+          description="This is my project in which I took several machine learning algorithms and convolutional neural network and passed them through the 'creditcard.csv' dataset. I noticed convolutional neural network performs better than the machine learning algorithms taken."
+          githubLink="https://github.com/shrihariss29/Credit-card-fraud-project"
+          backgroundColor="blanchedalmond"
+          imageSrc={require("../assets/Mldlpic.png")}
+        />
+        <ProjectCard
+          title="Portfolio website using React.js, HTML, and CSS"
+          description="This project is done using React.js primarily. This portfolio contains all the necessary information about myself. It is hosted via GitHub pages. This has the scope of many future add-ons which can be added in future."
+          githubLink="https://github.com/shrihariss29/Portfolio-2"
+          backgroundColor="#96EFFF"
+          imageSrc={require("../assets/Htmlpic.png")}
+        />
+        <ProjectCard
+          title="Data analysis using titanic.csv dataset"
+          description="Data analysis using the Titanic dataset is based on the sinking of Titanic ship. It consists of data of passengers who travelled in the ship during the accident. Based on the specific passenger's sex, age, ticket type, and other parameters, chances of their survival rate is calculated."
+          githubLink="https://github.com/shrihariss29/Titanic-dataset-project"
+          backgroundColor="#C7DCA7"
+          imageSrc={require("../assets/dapic.png")}
+        />
+        <ProjectCard
+          title="Machine learning - basic to advanced (Certification)"
+          description="This is a course where I started to learn about machine learning and data analytics. It is very fascinating to learn it because it feels like we are predicting the future. Learnt the deep intricacies and insights in this area."
+          githubLink={require("../assets/MLcertificate.png")}
+          backgroundColor="#DDE6ED"
+          imageSrc={require("../assets/decisiontree.png")}
+        />
+        <ProjectCard
+          title="Excel basics for data analytics (Certification)"
+          description="Working with Microsoft Excel is a very important skill for a data analyst. Learnt many applications of Excel and felt that it is a very underrated and useful tool. Had a chance to use it in my real life applications also."
+          githubLink={require("../assets/Excelcertificate.png")}
+          backgroundColor="#FFBF9B"
+          imageSrc={require("../assets/excellogo.jpg")}
+        />
       </div>
       <footer>Developed by Shrihari</footer>
-  </div>;
+    </div>
+  );
 };
 
 export default Projects;
+
